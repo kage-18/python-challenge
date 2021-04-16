a76a23935e1d5a54fae8e0e53ea139fd20185896
Karen Gutierrez <kareng018@berkeley.edu>
2021-04-15 22:59:14 -0700

    Attempting the output file

--- PyBank/main.py
+++ PyBank/main.py
@@ -34,4 +34,12 @@ Average Change: ${round(mean(changes), 2)}
 Greatest Increase in Profits: {max_increase[0]} (${max_increase[1]})
 Greatest Decrease in Profits: {max_decrease[0]} (${max_decrease[1]})
 ''')
-
+input_data = {"Total Months": months, 
+                "Total": net_total, 
+                "Average Change": round(mean(changes), 2), 
+                "Greatest Increase in Profits": [max_increase[0], max_increase[1]], 
+                "Greatest Decrease in Profits": [max_decrease[0], max_decrease[1]]}
+output_file_path = os.path.join("analysis", "analysis.csv")
+with open(output_file_path, 'w') as output_file:
+    csv_writer = csv.writer(output_file, delimiter=",")
+    csv_writer.writerow(input_data)
