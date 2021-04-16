92734a963ad55aba198d44599c2bfce6de807778
Karen Gutierrez <kareng018@berkeley.edu>
2021-04-15 22:31:42 -0700

    average change corrected and debugged

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,17 +4,21 @@ from statistics import mean
 
 file_path = os.path.join("Resources", "budget_data.csv")
 
-months = []
-net_total = 0
-max_increase = ["",0]
-max_decrease = ["",0]
 
 with open(file_path, 'r') as open_file:
     csv_reader = csv.reader(open_file, delimiter=",")
     next(csv_reader, None)
+    first_row = next(csv_reader, None)
+    months = 0
+    previous_val = int(first_row[1])
+    net_total = 0 + previous_val
+    max_increase = [first_row[0], previous_val]
+    max_decrease = [first_row[0], previous_val]
+    changes = []
     for row in csv_reader:
-        if row[0] not in months:
-            months.append(row[0])
+        months += 1
+        changes.append(int(row[1])-previous_val)
+        previous_val = int(row[1])
         net_total += int(row[1])
         if int(row[1]) >= max_increase[1]:
             max_increase = [row[0],int(row[1])]
@@ -24,10 +28,11 @@ with open(file_path, 'r') as open_file:
 print(f'''
 Financial Analysis
 ---------------------------
-Total Months:{len(months)}
+Total Months:{months}
 Total: ${net_total}
-Average Change: ${net_total/len(months)}
+Average Change: ${round(mean(changes), 2)}
 Greatest Increase in Profits: {max_increase[0]} (${max_increase[1]})
 Greatest Decrease in Profits: {max_decrease[0]} (${max_decrease[1]})
 ''')
         
+print(changes)
